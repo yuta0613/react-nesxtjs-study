b7de2b379ae6762e9fee76f4977ee7067d4cd33d
yuta0613 <ynikyeg0613@yahoo.co.jp>
2024-01-28 14:44:15 +0900

    update react 3-1 app

--- chapter-3/react_app_3-1/src/App.js
+++ chapter-3/react_app_3-1/src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+        <div>
+          <h1 className="bg-primary text-white display-4">React</h1>
+          <div className="container">
+            <p className="subtitle">Reactを使ったサンプルアプリです。</p>
+            <p>これはサンプルコンポーネントです</p>
+            <p>簡単なメッセージを表示します</p>
+          </div>
+        </div>
+    )
+  }
 }
 
 export default App;
