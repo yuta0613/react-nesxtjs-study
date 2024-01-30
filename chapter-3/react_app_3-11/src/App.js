72364b64beaaf006378eca15fe833c081ad01f83
yuta0613 <ynikyeg0613@yahoo.co.jp>
2024-01-31 01:24:06 +0900

    add chapter 3-4 react_apps

---
+++ chapter-3/react_app_3-11/src/App.js
@@ -0,0 +1,47 @@
+import React,{Component} from 'react';
+import './App.css';
+
+let data = {
+    title: "React-context",
+  message: "This is sample message."
+}
+
+const  SampleContext = React.createContext(data);
+
+class App extends Component {
+  render() {
+    return (
+        <div>
+          <h1 className="bg-primary text-white display-4">React</h1>
+            <div className="container">
+                <Title />
+                <Message />
+            </div>
+        </div>
+    );
+  }
+}
+
+class Title extends Component {
+  static contextType = SampleContext;
+  render() {
+    return (
+        <div className="card p-2 my-3">
+          <h2>{this.context.title}</h2>
+        </div>
+    );
+  }
+}
+
+class Message extends Component {
+  static contextType = SampleContext;
+  render() {
+    return (
+        <div className="alert alert-primary">
+          <p>{this.context.message}</p>
+        </div>
+    );
+  }
+}
+
+export default App;
