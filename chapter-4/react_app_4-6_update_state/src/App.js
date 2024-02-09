42c693c0859fb349b152809082cdfc26907d7463
yuta0613 <ynikyeg0613@yahoo.co.jp>
2024-02-10 03:54:46 +0900

    add list 4-16

--- chapter-4/react_app_4-6_update_state/src/App.js
+++ chapter-4/react_app_4-6_update_state/src/App.js
@@ -382,22 +382,71 @@ import './App.css';
 // export default App
 
 // 4-14 define original useState
-function useCounter() {
-    const [num, setNum] = useState(0)
+// function useCounter() {
+//     const [num, setNum] = useState(0)
+//
+//     const count = () =>  {
+//         setNum(num + 1)
+//     }
+//     return [num, count]
+// }
+//
+// function AlertMessage(props) {
+//     const [counter, plus] = useCounter()
+//     return <div className="alert alert-primary h5 text-center">
+//         <h4>count: {counter}</h4>
+//         <button onClick={plus} className="btn btn-primary">
+//             count
+//         </button>
+//     </div>
+// }
+//
+// function App() {
+//     return(
+//         <div>
+//             <h1 className="bg-primary text-white display-4">React</h1>
+//             <div className="container">
+//                 <h4 className="my-3">Hooks sample</h4>
+//                 <AlertMessage />
+//             </div>
+//         </div>
+//     )
+// }
+//
+// export default App
 
-    const count = () =>  {
-        setNum(num + 1)
+// 4-16 multiple use state
+const useTax = (t1, t2) => {
+    const [price, setPrice] = useState(1000)
+    const [tx1] = useState(t1)
+    const [tx2] = useState(t2)
+
+    const tax = () => {
+        return Math.floor(price * (1.0 + tx1 / 100))
     }
-    return [num, count]
+
+    const reduced = () => {
+        return Math.floor(price * (1.0 + tx2 / 100))
+    }
+
+    return [price, tax, reduced, setPrice]
 }
 
 function AlertMessage(props) {
-    const [counter, plus] = useCounter()
-    return <div className="alert alert-primary h5 text-center">
-        <h4>count: {counter}</h4>
-        <button onClick={plus} className="btn btn-primary">
-            count
-        </button>
+    const [price,tax, reduced, setPrice] = useTax(10,8)
+
+    const DoChange = (e) => {
+        let p = e.target.value
+        setPrice(p)
+    }
+
+    return <div className="alert alert-primary h5">
+        <p className="h5">通常税率: {tax()} 円</p>
+        <p className="h5">軽減税率: {reduced()} 円</p>
+        <div className="form-group">
+            <label className="form-group-label">Price:</label>
+            <input type="number" className="form-control" onChange={DoChange} value={price}/>
+        </div>
     </div>
 }
 
