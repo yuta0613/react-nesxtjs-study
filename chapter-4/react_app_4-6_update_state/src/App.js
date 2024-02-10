dfc7c8e46f3bb1248d88265909d6e7a097db374c
yuta0613 <ynikyeg0613@yahoo.co.jp>
2024-02-11 00:33:09 +0900

    add list 4-18

--- chapter-4/react_app_4-6_update_state/src/App.js
+++ chapter-4/react_app_4-6_update_state/src/App.js
@@ -416,50 +416,126 @@ import './App.css';
 // export default App
 
 // 4-16 multiple use state
-const useTax = (t1, t2) => {
-    const [price, setPrice] = useState(1000)
-    const [tx1] = useState(t1)
-    const [tx2] = useState(t2)
+// const useTax = (t1, t2) => {
+//     const [price, setPrice] = useState(1000)
+//     const [tx1] = useState(t1)
+//     const [tx2] = useState(t2)
+//
+//     const tax = () => {
+//         return Math.floor(price * (1.0 + tx1 / 100))
+//     }
+//
+//     const reduced = () => {
+//         return Math.floor(price * (1.0 + tx2 / 100))
+//     }
+//
+//     return [price, tax, reduced, setPrice]
+// }
+//
+// function AlertMessage(props) {
+//     const [price,tax, reduced, setPrice] = useTax(10,8)
+//
+//     const DoChange = (e) => {
+//         let p = e.target.value
+//         setPrice(p)
+//     }
+//
+//     return <div className="alert alert-primary h5">
+//         <p className="h5">通常税率: {tax()} 円</p>
+//         <p className="h5">軽減税率: {reduced()} 円</p>
+//         <div className="form-group">
+//             <label className="form-group-label">Price:</label>
+//             <input type="number" className="form-control" onChange={DoChange} value={price}/>
+//         </div>
+//     </div>
+// }
+//
+// function App() {
+//     return(
+//         <div>
+//             <h1 className="bg-primary text-white display-4">React</h1>
+//             <div className="container">
+//                 <h4 className="my-3">Hooks sample</h4>
+//                 <AlertMessage />
+//             </div>
+//         </div>
+//     )
+// }
+//
+// export default App
 
-    const tax = () => {
-        return Math.floor(price * (1.0 + tx1 / 100))
+// 4-18 use algorithm for hooks
+const total = (a) => {
+    let re = 0
+    for(let i = 0; i <=a;i++) {
+        re += i
     }
-
-    const reduced = () => {
-        return Math.floor(price * (1.0 + tx2 / 100))
-    }
-
-    return [price, tax, reduced, setPrice]
+    return re
 }
 
-function AlertMessage(props) {
-    const [price,tax, reduced, setPrice] = useTax(10,8)
+const tax =  (a) =>  {
+    return Math.floor(a * 1.1)
+}
 
-    const DoChange = (e) => {
-        let p = e.target.value
-        setPrice(p)
+function useCal(num=0, func= (a) => {return a}) {
+    const [msg, setMsg] = useState(null)
+
+    const  setValue= (p) => {
+        let res = func(p)
+        setMsg(<p className="h5"> {p}の結果は,{res}です</p> )
+    }
+    return [msg, setValue]
+}
+
+function  PlainMessage(props) {
+    const  [msg, setCal] = useCal()
+
+    const onChange = (e) => {
+        setCal(e.target.value)
     }
 
-    return <div className="alert alert-primary h5">
-        <p className="h5">通常税率: {tax()} 円</p>
-        <p className="h5">軽減税率: {reduced()} 円</p>
-        <div className="form-group">
-            <label className="form-group-label">Price:</label>
-            <input type="number" className="form-control" onChange={DoChange} value={price}/>
-        </div>
+    return <div className="p-3 h5">
+        <h5>{msg}</h5>
+        <input type="number" className="form-control" onChange={onChange}/>
+    </div>
+ }
+
+function AlertMessage(props) {
+    const [msg, setCals] = useCal(0, total)
+    const onChange = (e) => {
+    setCals(e.target.value)
+    }
+    return <div className="alert alert-primary h5 text-primary">
+        <h5>{msg}</h5>
+        <input type="number" className="form-control" onChange={onChange} min="0" max="10000"/>
     </div>
 }
 
-function App() {
-    return(
+function CardMessage(props) {
+    const [msg, setCalc] = useCal(0, tax)
+
+    const onChange  = (e) => {
+        setCalc(e.target.value)
+    }
+
+    return <div className="card p-3 h5 border-primary">
+        <h5>{msg}</h5>
+        <input type="range" onChange={onChange} min='0' max='10000' step='100' className='form-control'/>
+    </div>
+ }
+
+ function App() {
+    return (
         <div>
-            <h1 className="bg-primary text-white display-4">React</h1>
-            <div className="container">
+            <h1 className='bg-primary text-white display-4'>React</h1>
+            <div className='container'>
                 <h4 className="my-3">Hooks sample</h4>
+                <PlainMessage />
                 <AlertMessage />
+                <CardMessage />
             </div>
         </div>
     )
-}
+ }
 
-export default App
+ export default App
