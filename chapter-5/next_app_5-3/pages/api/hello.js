a7a88c1f8503a02354f9058d43a96c7c5e8a7e10
yuta0613 <ynikyeg0613@yahoo.co.jp>
2024-02-15 03:23:55 +0900

    make api

--- chapter-5/next_app_5-3/pages/api/hello.js
+++ chapter-5/next_app_5-3/pages/api/hello.js
@@ -1,5 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+import apiData from '../../components/data'
+
 export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+  let id = req.query.id
+  if (id === undefined) {id = 0}
+  if (id >= apiData.length) {id = 0}
+
+  res.json(apiData[id]);
 }
--- chapter-5/next_app_5-3/pages/index.js
+++ chapter-5/next_app_5-3/pages/index.js
@@ -37,35 +37,56 @@
 
 
 // 5-18 useSWR
+// import Layout from "../components/layout";
+// import useSWR from 'swr';
+//
+// const fetcher = (url) => fetch(url).then((res) => res.json());
+//
+// export default function Home() {
+//   const { data } = useSWR('./data.json', fetcher)
+//
+//   return (
+//       <div>
+//         <Layout header="Next.js" title="Top page.">
+//           <div className="alert alert-primary text-center">
+//             <h5 className="mb-4">
+//               {data != undefined ? data.message :'error...'}
+//             </h5>
+//             <table className="table table-dark">
+//               <thead className="">
+//               <tr><th>Name</th><th>Mail</th><th>Age</th></tr>
+//               </thead>
+//               <tbody>
+//               { data != undefined ? data.data.map((value, key)=> (
+//                   <tr key={key}>
+//                     <th>{value.name}</th>
+//                     <th>{value.mail}</th>
+//                     <th>{value.age}</th>
+//                   </tr>
+//               )) : <tr><th></th><td>no data.</td><td></td></tr>}
+//               </tbody>
+//             </table>
+//           </div>
+//         </Layout>
+//       </div>
+//   )
+// }
+
+// 5-20 pick up data.txt
 import Layout from "../components/layout";
 import useSWR from 'swr';
 
-const fetcher = (url) => fetch(url).then((res) => res.json());
-
 export default function Home() {
-  const { data } = useSWR('./data.json', fetcher)
+  const func = (...args) => fetch(...args).then(res => res.text())
+  const { data, err} = useSWR('./data.txt', func)
 
   return (
       <div>
         <Layout header="Next.js" title="Top page.">
           <div className="alert alert-primary text-center">
             <h5 className="mb-4">
-              {data != undefined ? data.message :'error...'}
+              {data}
             </h5>
-            <table className="table table-dark">
-              <thead className="">
-              <tr><th>Name</th><th>Mail</th><th>Age</th></tr>
-              </thead>
-              <tbody>
-              { data != undefined ? data.data.map((value, key)=> (
-                  <tr key={key}>
-                    <th>{value.name}</th>
-                    <th>{value.mail}</th>
-                    <th>{value.age}</th>
-                  </tr>
-              )) : <tr><th></th><td>no data.</td><td></td></tr>}
-              </tbody>
-            </table>
           </div>
         </Layout>
       </div>
