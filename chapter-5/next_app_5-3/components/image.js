2f5a72b607c56ba1f093c24797cb6484a850c810
yuta0613 <ynikyeg0613@yahoo.co.jp>
2024-02-15 03:06:24 +0900

    add useSWR fetch index.js

---
+++ chapter-5/next_app_5-3/components/image.js
@@ -0,0 +1,8 @@
+export default function MyImage(props) {
+    let fname = './' + props.fname
+    let size = props.size + 'px'
+
+    return (
+        <img width={size} border="1" src={fname}  alt="MyImage" />
+    )
+}
---
+++ chapter-5/next_app_5-3/pages/index.js
@@ -0,0 +1,73 @@
+// 5-17 fetch URL using fetch
+// import {useState} from "react";
+// import Layout from "../components/layout";
+//
+// export default function Home() {
+//   const url = './data.json'
+//   const [data, setData] = useState({message:'', data:[]})
+//
+//   fetch(url)
+//       .then(res=>res.json())
+//       .then(res => setData(res))
+//
+//   return (
+//       <div>
+//         <Layout header="Next.js" title="Top page">
+//           <div className="alert alert-primary text-center">
+//             <h5 className="mb-4">{data.message}</h5>
+//             <table className="table bg-white">
+//               <thead className="table-dark">
+//               <tr><th>Name</th><th>Mail</th><th>Age</th></tr>
+//               </thead>
+//               <tbody>
+//               {data.data.map((value, key)=> (
+//                   <tr key={key}>
+//                     <th>{value.name}</th>
+//                     <th>{value.mail}</th>
+//                     <th>{value.age}</th>
+//                   </tr>
+//               ))}
+//               </tbody>
+//             </table>
+//           </div>
+//         </Layout>
+//       </div>
+//   )
+// }
+
+
+// 5-18 useSWR
+import Layout from "../components/layout";
+import useSWR from 'swr';
+
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
+export default function Home() {
+  const { data } = useSWR('./data.json', fetcher)
+
+  return (
+      <div>
+        <Layout header="Next.js" title="Top page.">
+          <div className="alert alert-primary text-center">
+            <h5 className="mb-4">
+              {data != undefined ? data.message :'error...'}
+            </h5>
+            <table className="table table-dark">
+              <thead className="">
+              <tr><th>Name</th><th>Mail</th><th>Age</th></tr>
+              </thead>
+              <tbody>
+              { data != undefined ? data.data.map((value, key)=> (
+                  <tr key={key}>
+                    <th>{value.name}</th>
+                    <th>{value.mail}</th>
+                    <th>{value.age}</th>
+                  </tr>
+              )) : <tr><th></th><td>no data.</td><td></td></tr>}
+              </tbody>
+            </table>
+          </div>
+        </Layout>
+      </div>
+  )
+}
